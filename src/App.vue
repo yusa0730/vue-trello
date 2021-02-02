d774bb6fb1fcf073d7c112b5044ea2ad1b08931b
yusa0730 <isizawa5226@gmail.com>
2021-02-02 13:34:33 +0900

    first-commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <!-- ★Boardコンポーネント呼び出し --> 
+    <!-- componentsに登録した名前で呼び出すことができます。 -->
+    <board/>
+  </div>
+</template>
+
+<script>
+// コンポーネントを呼び出すときの名前は基本的にファイル名で定義します。
+import Board from './components/Board.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    // ★コンポーネントに登録する
+    // テンプレートでコンポーネントとして、呼び出すためにcomponentsプロパティへ名前を登録します。
+    Board
+  }
+}
+</script>
---
+++ src/components/Board.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <header>
+      my Trello
+    </header>
+    <main>
+      <p class="info-line">All: 0 tasks</p>
+    </main>
+  </div>
+</template>
+
+<script>
+  // export defaultと宣言することで、このコンポーネントを他のコンポーネントからimportして使用できるようになります。
+  export default {
+    
+  }
+</script>
