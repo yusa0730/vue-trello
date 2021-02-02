4de5fa7ef06f688e270fe1dce56cad0c86cad1f7
yusa0730 <isizawa5226@gmail.com>
2021-02-03 02:27:39 +0900

    修正

--- src/components/ListAdd.vue
+++ src/components/ListAdd.vue
@@ -1,58 +1,71 @@
 <template>
-<!-- 
-サブミットイベント時にaddListメソッドをハンドルしています。
-v-onディレクティブでイベントをハンドルでき、@はv-onの省略記法になります。
-また、.preventはVueのイベント修飾詞であり、サブミット時にリロードされるのを防ぎます。 
--->
-  <form class="addlist" @submit.prevent="addlist">
+  <!-- 
+  サブミットイベント時にaddListメソッドをハンドルしています。
+  v-onディレクティブでイベントをハンドルでき、@はv-onの省略記法になります。
+  また、.preventはVueのイベント修飾詞であり、サブミット時にリロードされるのを防ぎます。 
+  -->
+  <!-- v-bindの省略記法が : となります。 -->
+  <form :class="classList" @submit.prevent="addList">
     <!-- v-modelにtitleを定義することで、dataプロパティとバインドさせます。
-
     データをバインドさせる(データバインディング)とは、
     scriptで定義したdataプロパティをテンプレートでもリアルタイムにそのまま共有（紐付け）できるような仕組みです。
-
     ここで使うv-modelはv-onとv-bindの2つの機能を持ち合わせています。 
     v-modelは今回のようなinputやselect、textareaなどのフォーム要素に対して使うことができます。
     -->
     <input v-model="title"
-          type="text"
-          class="text-input"
-          placeholder="Add new list"
+           type="text"
+           class="text-input"
+           placeholder="Add new list"
+           @focusin="startEditing"
+           @focusout="finishEditing"
     >
-    <button type="submit" class="add-button">
+    <button type="submit"
+            class="add-button"
+            v-if="isEditing || titleExists">
       Add
     </button>
   </form>
 </template>
 
 <script>
-  export default {
-    data: function() {
-      return {
-        title: "",
-      }
-    },
-    methods: {
-      addList: function(){
-        // this.$store.dispatch('アクション名')でactionsを実行できます。
-        // 第二引数にオブジェクト型でdataプロパティのtitleを渡します。
-        this.$store.dispatch('addlist', { title:this.title })
-        this.title =""
-      }
-    },
-  }
-</script>
-
-<script>
 export default {
   data: function() {
     return {
       title: '',
+      // isEditingというフラグを立てて、trueの時（フォーカスされている時）だけstyleを追加する
+      isEditing: false,
     }
   },
+  // watchでも同じ処理を行うことができるが
+  // watchは、データの変更があれば特定の関数を実行する。など少し複雑な処理を行いたいときに利用するのがよい。
+  computed: {
+    classList() {
+      const classList = ['addlist']
+
+      if (this.isEditing) {
+        classList.push('active')
+      }
+      if (this.titleExists) {
+        classList.push('addable')
+      }
+      return classList
+    },
+    titleExists() {
+      return this.title.length > 0
+    },
+  },
   methods: {
-    addList: function() {
+    addList() {
+      // this.$store.dispatch('アクション名')でindex.jsのactionsを実行できます。
+      // 第二引数にオブジェクト型でdataプロパティのtitleを渡します。
       this.$store.dispatch('addlist', { title: this.title })
       this.title = ''
+    },
+    startEditing() {
+      this.isEditing = true
+    },
+    finishEditing() {
+      this.isEditing = false
     },
   }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -10,8 +10,7 @@ Vue.use(Vuex)
 // localStorage.getItem('設定したキー')でlocalStorageに保存したリストを取得します。
 const savedLists = localStorage.getItem('trello-lists')
 
-// ストアインスタンスを取得
-const store = new Vuex.Store({
+const store =  new Vuex.Store({
   // mutationsでリストのデータの状態を更新できるように、stateにリストを定義
   state: {
     // localStorageに保存されたリストがあれば取得、なければデフォルトのリスト配列を設置します。
@@ -41,31 +40,21 @@ const store = new Vuex.Store({
   // mutationsの重要なルールとして、同期的でなければならない
   // mutationsはあくまでもストアの状態の変更だけを行う。mutationsの呼び出しはactionsが行う
   mutations: {
-    // addListメソッドはstateのデータへリストの追加を行う
-    // mutationsは第一引数でstate、第二引数ではコミット時に受け取る引数payloadを指定できます
     addlist(state, payload) {
-      state.list.push({ title: payload.title, cards:[] })
-    }
+      state.lists.push({ title: payload.title, cards:[] })
+    },
   },
   actions: {
-    // actionsは第一引数にcontextというストアインスタンスのメソッドやプロパティを呼び出せるオブジェクトを受け取ることができます
-    // 第二引数には、mutationsに渡す引数を指定できます。
     addlist(context, payload) {
       context.commit('addlist', payload)
     },
   },
-  // 分割したファイル名をmodulesでまとめて定義してストアインスタンスを生成します。
-  // 今回はストアの定義は1ファイルだけなのでmodulesの代わりにgettersを定義
   getters: {
-  }
+  },
 })
 
-// subscribeはストアのインスタンスメソッドで、全てのmutationの後に呼ばれます。
-// 第一引数にmutationインスタンス、第二引数にmutation後のデータの状態を受け取ります。
 store.subscribe((mutation, state) => {
-  // // localStorage.setItem('設定するキー', 文字列型のデータ)でデータの状態を更新後にlocalStorageへデータの状態を保存しています。
   localStorage.setItem('trello-lists', JSON.stringify(state.lists))
 })
 
-// main.jsでインポートできるようにexport defaultしています。
 export default store
